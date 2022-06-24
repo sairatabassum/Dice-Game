fe43893ab2bd0cba033e0479063d2b521559091a
Saira Tabassum <sairatabassum386@gmail.com>
2022-06-24 13:38:54 +0600

    commit

--- script.js
+++ script.js
@@ -18,27 +18,73 @@ dice.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  currentScore = 0;
+  player0.classList.toggle('player--active'); //because on player 0 it will remove the class if it's there and if it's not it will add it.
+  player1.classList.toggle('player--active');
+}
+
 // Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
+  if (playing) {
+    // 1. Generating a random dice roll
 
-  const diceNumber = Math.trunc(Math.random() * 6) + 1;
-  // 2. Display dice
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display dice
 
-  dice.classList.remove('hidden');
-  dice.src = `dice-${diceNumber}.png`;
+    dice.classList.remove('hidden');
+    dice.src = `dice-${diceNumber}.png`;
 
-  // 3. Check for rolled 1: if true then switch to new player
-  if (diceNumber === 1) {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    currentScore = 0;
-    player0.classList.toggle('player--active'); //because on player 0 it will remove the class if it's there and if it's not it will add it.
-    player1.classList.toggle('player--active');
-  } else {
-    currentScore += diceNumber;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    // 3. Check for rolled 1: if true then switch to new player
+    if (diceNumber === 1) {
+      switchPlayer();
+    } else {
+      currentScore += diceNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
   }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  scores[0] = 0;
+  scores[1] = 1;
+  currentScore0.textContent = 0;
+  currentScore1.textContent = 0;
+  score0.textContent = 0;
+  score1.textContent = 0;
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+  dice.classList.add('hidden');
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 });
